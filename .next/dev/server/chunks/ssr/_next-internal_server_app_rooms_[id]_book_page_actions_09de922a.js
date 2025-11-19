3d69fe85649312b3700258308f98629adc7f3fe2
Tristan LE MEE <tristan.lemee22@gmail.com>
2025-11-19 13:43:09 +0100

    fix ignore .next

--- .next/dev/server/chunks/ssr/_next-internal_server_app_rooms_[id]_book_page_actions_09de922a.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/rooms/[id]/book/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_rooms_%5Bid%5D_book_page_actions_09de922a.js.map
